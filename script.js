1162039f97b1aec5d039c3e94356e3092e58fe86
Ayush Mann <mannayush60@gmail.com>
2024-08-13 14:32:14 +0530

    updated

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-var points = 0;
+var points = 100;
 var moneySpent = 0;
+var timeSpent = 1;
 var moneyExpendition = document.getElementById('moneyExpendition');
 var addExpenseButton = document.getElementById('addExpenseButton');
 var removeExpenseButton = document.getElementById('removeExpenseButton');
-var expenseInput = document.getElementById('expenseInput');
-moneyExpendition.value = moneySpent;
 
+var timeExpendition = document.getElementById('timeExpendition');
+var addTimeButton = document.getElementById('addTimeButton');
+var removeTimeButton = document.getElementById('removeTimeButton');
+
+var expenseInput = document.getElementById('expenseInput');
+var profilePoints = document.getElementById('profilePoints');
+moneyExpendition.value = moneySpent;
+timeExpendition.value = timeSpent;
 
 function addMoney(){
     moneySpent += parseInt(expenseInput.value);
     moneyExpendition.value = moneySpent;
+    points -= moneySpent * 0.1;
 };
 
 function removeMoney(){
@@ -19,9 +27,29 @@ function removeMoney(){
     else{
     moneySpent -= parseInt(expenseInput.value);
     moneyExpendition.value = moneySpent;
+    points -= moneySpent * 0.1;
 }};
 
+
 function conversion(moneySpent){
-    points = moneySpent * 0.1;
+    profilePoints.value = points;
     return points;
 };
+
+setInterval(function() {
+    conversion(moneySpent);
+    if (points <= 0){
+        alert('You have no more points. Please spend money wisely.');
+        breakloop;
+    };
+}, 1000);
+
+
+
+function addTime(){
+    alert('You are in ZEN MODE and cannot use laptop.')
+    timeSpent += parseInt(timeInput.value);
+    timeExpendition.value = timeSpent;
+    points += timeSpent;
+};
+
